c9a92412584d04b077ace56a62a2f4fb553ef9a6
Opilane TTHK <opilane@tthk.ee>
2022-09-15 13:50:23 +0300

    refvsout

---
+++ RefVSOut/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RefVsOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int first = 1;
+            int second = 2;
+
+            Method1(out first, out second);
+            Method2(first, ref second);
+            Method3(ref first, second);
+
+            Console.WriteLine($"first: {first}, second: {second}");
+            Console.ReadLine();
+        }
+        static void Method1(out int a, out int b)
+        {
+            a = 2;
+            b = 1;
+
+        }
+
+        static void Method2(int a, ref int b)
+        {
+            a *= 5;
+            b *= a;
+        }
+
+        static void Method3(ref int a, int b)
+        {
+            a *= 5;
+            b *= b;
+        }
+    }
+}
